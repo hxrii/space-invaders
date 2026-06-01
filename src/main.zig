a589f3ab3e248b67c2a1163d8426444c9d4bd931
hari <harikrishnankannoth@gmail.com>
2026-06-01 22:39:24 +0530

    keyboard movements

--- src/main.zig
+++ src/main.zig
@@ -7,14 +7,33 @@ const Rectangle = struct {
     height: f32,
 
     pub fn intersects(self: Rectangle, other: Rectangle) bool {
-        return self.x < other.x + self.width and
+        return self.x < other.x + other.width and
             self.x + self.width > other.x and
             self.y < other.y + other.height and
             self.y + self.height > other.y;
     }
 };
 
-const GameConfig = struct { screenWidth: i32, screenHeight: i32, playerWidth: i32, playerHeight: i32, playerStartY: i32, bulletWidth: f32, bulletHeight: f32, shieldStartX: f32, shieldY: f32, shieldWidth: f32, shieldHeight: f32, shieldSpacing: f32, invaderStartX: f32, invaderStartY: f32, invaderHeightX: f32, invaderHeightY: f32, InvaderSpacingX: f32, InvaderSpacingY: f32 };
+const GameConfig = struct {
+    screenWidth: i32,
+    screenHeight: i32,
+    playerWidth: i32,
+    playerHeight: i32,
+    playerStartY: i32,
+    bulletWidth: f32,
+    bulletHeight: f32,
+    shieldStartX: f32,
+    shieldY: f32,
+    shieldWidth: f32,
+    shieldHeight: f32,
+    shieldSpacing: f32,
+    invaderStartX: f32,
+    invaderStartY: f32,
+    invaderHeightX: f32,
+    invaderHeightY: f32,
+    InvaderSpacingX: f32,
+    InvaderSpacingY: f32,
+};
 
 const Player = struct {
     position_x: f32,
@@ -23,17 +42,30 @@ const Player = struct {
     height: f32,
     speed: f32,
 
-    pub fn init(position_x:f32, position_y:f32, width: f32, height: f32) @This(){
-        return Player{
-            .position_x:f32 = position_x,
-            .position_y:f32 = position_y,
-            .width:f32 = width,
-            .height:f32 = height
+    pub fn init(position_x: f32, position_y: f32, width: f32, height: f32) @This() {
+        return Player{ .position_x = position_x, .position_y = position_y, .width = width, .height = height, .speed = 5.0 };
+    }
+
+    pub fn update(self: *@This()) void {
+        if (rl.isKeyDown(rl.KeyboardKey.right)) {
+            self.position_x += self.speed;
+        }
+
+        if (rl.isKeyDown(rl.KeyboardKey.left)) {
+            self.position_x -= self.speed;
         }
     }
+
+    pub fn draw(self: @This()) void {
+        rl.drawRectangle(
+            @intFromFloat(self.position_x),
+            @intFromFloat(self.position_y),
+            @intFromFloat(self.width),
+            @intFromFloat(self.height),
+            rl.Color.blue,
+        );
+    }
 };
-
-
 
 pub fn main() void {
     const screen_width = 800;
@@ -42,6 +74,16 @@ pub fn main() void {
     rl.initWindow(screen_width, screen_height, "Zig Invaders");
 
     defer rl.closeWindow();
+
+    const player_width = 50.0;
+    const player_height = 30.0;
+    var player = Player.init(
+        @as(f32, @floatFromInt(screen_width)) / 2 - player_width / 2,
+        @as(f32, @floatFromInt(screen_height)) - 60.0,
+        player_width,
+        player_height,
+    );
+
     rl.setTargetFPS(60);
 
     while (!rl.windowShouldClose()) {
@@ -49,6 +91,8 @@ pub fn main() void {
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.black);
+
+        player.update();
         rl.drawText("Zig Invaders", 300, 250, 40, rl.Color.green);
     }
 
