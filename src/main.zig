890cea97849f68e0a77cd1c536ca6b26140c1a71
hari <harikrishnankannoth@gmail.com>
2026-06-01 21:29:38 +0530

    Player Config

--- src/main.zig
+++ src/main.zig
@@ -14,7 +14,26 @@ const Rectangle = struct {
     }
 };
 
-const gameConfig = struct { screenWidth: i32, screenHeight: i32, playerWidth: i32, playerHeight: i32, playerStartY: i32, bulletWidth: f32, bulletHeight: f32, shieldStartX: f32, shieldY: f32, shieldWidth: f32, shieldHeight: f32, shieldSpacing: f32, invaderStartX: f32, invaderStartY: f32, invaderHeightX: f32, invaderHeightY: f32, InvaderSpacingX: f32, InvaderSpacingY: f32 };
+const GameConfig = struct { screenWidth: i32, screenHeight: i32, playerWidth: i32, playerHeight: i32, playerStartY: i32, bulletWidth: f32, bulletHeight: f32, shieldStartX: f32, shieldY: f32, shieldWidth: f32, shieldHeight: f32, shieldSpacing: f32, invaderStartX: f32, invaderStartY: f32, invaderHeightX: f32, invaderHeightY: f32, InvaderSpacingX: f32, InvaderSpacingY: f32 };
+
+const Player = struct {
+    position_x: f32,
+    position_y: f32,
+    width: f32,
+    height: f32,
+    speed: f32,
+
+    pub fn init(position_x:f32, position_y:f32, width: f32, height: f32) @This(){
+        return Player{
+            .position_x:f32 = position_x,
+            .position_y:f32 = position_y,
+            .width:f32 = width,
+            .height:f32 = height
+        }
+    }
+};
+
+
 
 pub fn main() void {
     const screen_width = 800;
