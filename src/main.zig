fdca19af70b4b4ad3ae36cf66863943c331c9072
hari <harikrishnankannoth@gmail.com>
2026-06-01 20:08:50 +0530

    Title Window done

--- src/main.zig
+++ src/main.zig
@@ -1,1 +1,21 @@
 const rl = @import("raylib");
+
+pub fn main() void {
+    const screen_width = 800;
+    const screen_height = 600;
+
+    rl.initWindow(screen_width, screen_height, "Zig Invaders");
+
+    defer rl.closeWindow();
+    rl.setTargetFPS(60);
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(rl.Color.black);
+        rl.drawText("Zig Invaders", 300, 250, 40, rl.Color.green);
+    }
+
+    return;
+}
